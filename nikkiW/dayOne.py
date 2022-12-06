a0a7cedf1297c015265705086f649ad63a791461
Nikki W <86427717+anikki1@users.noreply.github.com>
2022-12-06 10:34:38 -0500

    dayOne complete

---
+++ nikkiW/dayOne.py
@@ -0,0 +1,21 @@
+with open("data.txt", "r") as elfData:
+	lines = elfData.readlines()
+elfFood = []
+for x in lines: 
+    x = x.replace('\n', '')
+    if x.isnumeric(): 
+        elfFood[-1].append(int(x))
+        #print(x)
+    else:
+        elfFood.append([])
+        #print("newElf")
+totalFoodPerElf= []
+for y in elfFood:
+    totalFoodPerElf.append(sum(y))
+    #print(len(y))
+totalFoodPerElf.sort(reverse=True)
+print(totalFoodPerElf[0])
+
+topThreeElves = totalFoodPerElf[0:3]
+print(sum(topThreeElves))
+
